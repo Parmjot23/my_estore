6130cb19dc9a9b77087ed96bb2d4b6c869badc36
Parmjot23 <150370989+Parmjot23@users.noreply.github.com>
2025-06-17 13:57:30 -0400

    Prepare deployment for Netlify and PythonAnywhere

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'staticfiles' # For production 'collectstatic'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # STATICFILES_DIRS = [BASE_DIR / 'static'] # For development static files outside apps
 
 # Media files (User-uploaded content like product images)
