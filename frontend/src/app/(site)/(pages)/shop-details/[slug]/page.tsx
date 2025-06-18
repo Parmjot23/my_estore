04202c8c8cd5ffec88a3fb998bbf1d456f189b64
Parmjot23 <150370989+Parmjot23@users.noreply.github.com>
2025-06-17 22:32:31 -0400

    fix type for product details page

--- frontend/src/app/(site)/(pages)/shop-details/[slug]/page.tsx
+++ frontend/src/app/(site)/(pages)/shop-details/[slug]/page.tsx
@@ -3,14 +3,12 @@ import React from 'react';
 import ShopDetailsComponent from '@/components/ShopDetails'; // Assuming your main component is named ShopDetails
 import { getProductBySlug } from '@/lib/apiService';
 import { Product } from '@/types/product';
-import type { Metadata, ResolvingMetadata } from 'next';
+import type { Metadata, ResolvingMetadata, PageProps } from 'next';
 import APITestComponent from '@/components/Common/APITestComponent'; // For easy debugging
 
-type ProductDetailsPageProps = {
-  params: {
-    slug: string;
-  };
-};
+type ProductDetailsPageProps = PageProps<{
+  slug: string;
+}>;
 
 // Function to generate metadata dynamically
 export async function generateMetadata(
