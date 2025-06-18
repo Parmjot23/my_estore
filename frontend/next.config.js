46de67743b1351a898ac83bf25f0a4b69b23ff0d
Parmjot23 <150370989+Parmjot23@users.noreply.github.com>
2025-06-18 19:06:38 -0400

    fix image domain paths

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
         protocol: 'http',
         hostname: 'localhost',
         port: '8000',
-        pathname: '/uploads/**',
+        pathname: '/media/**',
       },
       {
         protocol: 'https',
@@ -24,7 +24,7 @@ if (process.env.NEXT_PUBLIC_BACKEND_HOST) {
   nextConfig.images.remotePatterns.push({
     protocol: 'https',
     hostname: process.env.NEXT_PUBLIC_BACKEND_HOST,
-    pathname: '/media/uploads/**',
+    pathname: '/media/**',
   });
 }
 
