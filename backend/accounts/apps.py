c74953d97bea3f191033d27ca2210b4352bc8941
Parmjot23 <150370989+Parmjot23@users.noreply.github.com>
2025-06-20 20:57:42 -0400

    Move admin site header setup to ready method

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+
+    def ready(self):
+        from django.contrib import admin
+        admin.site.site_header = 'Celtrix Admin'
+        admin.site.site_title = 'Celtrix Admin Portal'
+        admin.site.index_title = 'Welcome to the Celtrix Admin Panel'
