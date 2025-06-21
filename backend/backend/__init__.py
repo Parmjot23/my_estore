fcac0a7bfb54534da908f9f962bc3f21200895b3
Parmjot23 <150370989+Parmjot23@users.noreply.github.com>
2025-06-20 20:31:57 -0400

    Update admin branding to Celtrix

---
+++ backend/backend/__init__.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+admin.site.site_header = 'Celtrix Admin'
+admin.site.site_title = 'Celtrix Admin Portal'
+admin.site.index_title = 'Welcome to the Celtrix Admin Panel'
